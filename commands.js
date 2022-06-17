d532c9c903a20bed2f896487f2be65745e344e02
Sergio <sergioh182@gmail.com>
2022-06-16 21:56:56 -0700

    Add to 'projects' command

--- commands.js
+++ commands.js
@@ -10,15 +10,19 @@ let projectList = [
     "<br>",
     "<span class='cmdOutput'>Random Number Generator</span><span class='helpSpace'><a href='https://github.com/SHJR1/random-number-generator' target='_blank'>github</a></span><span class='helpSpace'><a href='https://codepen.io/sergioh1/full/LYQOQrd' target='_blank'>codepen</a></span><br>",
     "<br>",
+    "<span class='cmdOutput'>Portfolio Website</span><span class='helpSpace'><a href='https://shjr1.github.io/portfolio-website/'>live demo</a></span><span class='helpSpace'><a href='https://github.com/SHJR1/portfolio-website'>github</a></span><br>",
+    "<br>",
+    "<span class='cmdOutput'>This Website</span><span class='helpSpace'><a href='https://github.com/SHJR1/terminal-portfolio'>github</a></span><br>",
+    "<br>",
     "<span class='cmdOutput'>Random Password Generator</span><span class='helpSpace'><a href='#'>coming soon...</a></span><br>",
 ]
 
 let socialsList = [
     "<span class='helpSpace'><a href='https://github.com/SHJR1' target='_blank'><i class='fa-brands fa-github fa-xl'></i>Github</a></span>",
-    "<span class='helpSpace'><a href='https://codepen.io/sergioh1/full/LYQOQrd' target='_blank'><i class='fa-brands fa-codepen fa-xl'></i>Codepen</a></span>",
+    "<span class='helpSpace'><a href='https://codepen.io/sergioh1/' target='_blank'><i class='fa-brands fa-codepen fa-xl'></i>Codepen</a></span>",
     "<span class='helpSpace'><a href='https://www.linkedin.com/in/sergiodev/' target='_blank'><i class='fa-brands fa-linkedin fa-xl'></i>Linkedin</a></span>",
 ];
 
 let websiteList = [
-    "<span class='helpSpace'><a href='#'>website.com</a></span>",
+    "<span class='helpSpace'><a href='https://shjr1.github.io/portfolio-website/'>https://shjr1.github.io/portfolio-website/</a></span>",
 ];
